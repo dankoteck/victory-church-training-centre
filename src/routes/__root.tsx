82c6104891ecca34d40cf6d8c85c07eca1c5c472
khoalebita <138424647+khoalebita@users.noreply.github.com>
2024-05-13 23:18:21 +0700

    feat(init): initial commit source & done for setup @tanstack/router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,16 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: Page,
+});
+
+function Page() {
+  return (
+    <>
+      
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
